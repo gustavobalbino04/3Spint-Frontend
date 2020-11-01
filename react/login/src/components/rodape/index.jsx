c89b4910bc6bb61913b1dbce0eef9fa95be9c485
gustavobalbino04 <gubalbino15@gmail.com>
2020-11-01 13:29:29 -0300

    concluido

---
+++ react/login/src/components/rodape/index.jsx
@@ -0,0 +1,14 @@
+//importei o react para o meu webcomponentes
+import React from 'react';
+
+//Criei uma function basica pois a mesma só irá
+//apresentar informações,não irá receber parametros
+function Rodape() {
+    return(
+        <footer>
+      <h1>Senai Informática 132</h1>
+      </footer>
+    )
+}
+//Deixo o webcomponet visivel aos outros webcomponents
+export default Rodape;
